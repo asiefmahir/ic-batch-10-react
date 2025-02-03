6721a43b304f0cad07772963246b36bd372f9190
Asief Mahir <asiefmahir1@gmail.com>
2025-02-04 05:24:05 +0600

    feat: Learn about useState hook and how it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,66 @@
 import "./App.css";
-import BioData from "./components/BioData";
+import { useState } from "react";
 
-const bioDataInfos = [
-	{
-		name: "Mahir Asief",
-		email: "asiefmahir1@gmail.com",
-		skills: ["React", "Redux", "Next", "NodeJs"],
-		interests: ["Reading", "Writing"],
-	},
-	{
-		name: "Sr Setu",
-		email: "srsetu@gmail.com",
-		phone: "+1234567890",
-		skills: ["React", "Redux", "Next", "NodeJs", "WP", "PHP"],
-	},
-];
 function App() {
-	console.log("App is invoked");
+	// console.log("App is invoked");
+	// let counter = 0;
+	console.log("App Function is being Invoked");
+
+	// State
+	// Render
+	// Re-Render
+
+	const [dynamicCounter, setDynamicCounter] = useState(10);
+	const [name, setName] = useState("Mahir");
+	// dynamicCounter = 12
+
+	// console.log(janiNah, "janinah");
+
+	const increaseHandler = (value) => {
+		console.log("Increase Handler Triggered");
+		// dynamicCounter++
+		// setDynamicCounter(20);
+		setDynamicCounter(dynamicCounter + value);
+		// dynamicCounter + 1 = 10 + 1 = 11
+
+		// setDynamicCounter(11 + 1)
+		// setDynamicCounter(12)
+		// dynamicCounter = 12
+		// App()
+	};
+
+	const decreaseHandler = (value) => {
+		console.log(`Decrease Handler triggered`);
+		// counter--;
+		// dynamicCounter--
+		setDynamicCounter(dynamicCounter - value);
+		// console.log(counter);
+	};
+
+	// decreaseHandler()
+
+	const changeNameHandler = () => {
+		setName("Mahir Asief");
+	};
 
 	return (
 		<>
-			{bioDataInfos.map((bioData) => (
-				<>
-					<BioData
-						key={bioData.email}
-						name={bioData.name}
-						email={bioData.email}
-						phone={bioData.phone}
-						skills={bioData.skills}
-						interests={bioData.interests}
-					/>
-					<hr />
-				</>
-			))}
-			{/* <BioData
-				name="Mahir Asief"
-				email="asiefmahir@gmail.com"
-				skills={["React", "Redux", "Next", "NodeJs"]}
-				interests={["Reading", "Writing"]}
-			/>
-			<hr />
-			<BioData
-				name="Sr Setu"
-				email="srsetu@gmail.com"
-				phone="+1234567890"
-				skills={["React", "Redux", "Next", "NodeJs", "WP", "PHP"]}
-				// interests={["Reading", "Writing", "Playing", "CHESS"]}
-			/> */}
+			{/* <BioData /> */}
+			<h2>Hello React</h2>
+			<p>The value of the counter is {dynamicCounter}</p>
+			<p>hello {name}</p>
+			<button onClick={() => increaseHandler(1)}>Increase By 1</button>
+			<button onClick={() => decreaseHandler(1)}>Decrease By 1</button>
+			<button onClick={() => increaseHandler(5)}>Increase By 5</button>
+			<button onClick={() => decreaseHandler(3)}>Decrease By 3</button>
+			<button onClick={changeNameHandler}>Change Name</button>
 		</>
 	);
 }
+// let counter = 10;
+// document.getElementById("increase").addEventListener("click", () => {
+// 	counter++;
+// });
 
 // BioData()
 // App()
