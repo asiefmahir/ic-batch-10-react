5046eaafebf4b7a9be9d38179e40e7bee295dc8c
Asief Mahir <asiefmahir1@gmail.com>
2025-02-25 00:13:16 +0600

    feat: Learn about performance optimization techniques in react

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import './index.css'
 import App from "./App.jsx";
-import NoteProvider from "./contexts/Note.jsx";
-import App2 from "./App2.jsx";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-	<NoteProvider>
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
 		<App />
-	</NoteProvider>,
-	// <App2 />,
-	// <div>
-	// 	<p></p>
-	// </div>
+	</StrictMode>,
 );
-
-// App();
-
-// react -> react dom -> dom
