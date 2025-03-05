24727cb0b439af5fcddbedea42af50820316b765
Asief Mahir <asiefmahir1@gmail.com>
2025-03-05 15:30:40 +0600

    feat: Learn about custom hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,12 @@ import { useState, useEffect } from "react";
 // add()
 
 function App() {
-	const [posts, setPosts] = useState([]);
 	const [noteTitle, setNoteTitle] = useState("");
-	const [counter, setCounter] = useState(10);
-	const [counter2, setCounter2] = useState(25);
 
 	const [notes, setNotes] = useState([]);
 
 	const getAllNotes = async () => {
-		fetch(`http://localhost:3000/notes`)
+		fetch(`http://localhost:4000/notes`)
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data, "data");
@@ -24,28 +21,11 @@ function App() {
 	};
 
 	useEffect(() => {
-		console.log("I am inside useEffect");
-		fetch(`http://localhost:3000/posts`)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data, "data");
-				setPosts(data);
-			});
-	}, []);
-
-	useEffect(() => {
 		console.log("I am inside notes effect");
 		getAllNotes();
 	}, []);
 
 	console.log("I am outside fetch");
-	// posts = data
-	// fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5`)
-	// 	.then((res) => res.json())
-	// 	.then((data) => {
-	// 		console.log(data, "data");
-	// 		setPosts(data);
-	// 	});
 
 	const submitHandler = (e) => {
 		e.preventDefault();
@@ -71,26 +51,6 @@ function App() {
 
 	return (
 		<>
-			<div className="counter">
-				<p>The value of the counter is {counter}</p>
-				<button onClick={() => setCounter(counter + 1)}>
-					Increase By 1
-				</button>
-			</div>
-			<div className="counter">
-				<p>The value of the counter is {counter2}</p>
-				<button onClick={() => setCounter2(counter2 + 5)}>
-					Increase By 5
-				</button>
-			</div>
-			<div className="post-list">
-				<h2>All Posts</h2>
-				<ul>
-					{posts.map((post) => (
-						<li key={post.id}>{post.title}</li>
-					))}
-				</ul>
-			</div>
 			<div className="post-list">
 				<form onSubmit={submitHandler}>
 					<input
--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router";
+import { useFetch } from "../hooks/useFetch";
+
 const PostList = () => {
+	const { data: posts } = useFetch(
+		"https://jsonplaceholder.typicode.com/posts?_limit=5",
+		[],
+	);
+	// 30 minutes
+	// stack -> LIFO
+
 	return (
 		<div>
-			<h2>PostList</h2>
+			<h2>All Posts</h2>
+			{/* {errorMessage && <h2>{errorMessage}</h2>} */}
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>
+						<Link to={`/posts/${post.id}`}>{post.title}</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
