4e8bb6377942b3093e3560ac5c7fc0942e56a157
Asief Mahir <asiefmahir1@gmail.com>
2025-03-15 23:50:39 +0600

    feat: Learn about createAsyncThunk in Redux Toolkit

--- src/store/reducers/shop.js
+++ src/store/reducers/shop.js
@@ -1,38 +1,77 @@
+// const initState = {
+// 	isLoading: true,
+// 	errorMessage: "",
+// 	products: [],
+// };
+
+// export const shopReducer = (state = initState, action) => {
+// 	switch (action.type) {
+// 		case "shop/fetStart": {
+// 			return {
+// 				...state,
+// 				isLoading: true,
+// 			};
+// 		}
+
+// 		case "shop/fetchSucceeded": {
+// 			return {
+// 				...state,
+// 				isLoading: false,
+// 				products: action.payload,
+// 				errorMessage: "",
+// 			};
+// 		}
+
+// 		case "shop/fetchFailed": {
+// 			return {
+// 				...state,
+// 				isLoading: false,
+// 				errorMessage: action.payload,
+// 				products: [],
+// 			};
+// 		}
+
+// 		default: {
+// 			return state;
+// 		}
+// 	}
+// };
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
 const initState = {
 	isLoading: true,
 	errorMessage: "",
 	products: [],
 };
 
-export const shopReducer = (state = initState, action) => {
-	switch (action.type) {
-		case "shop/fetStart": {
-			return {
-				...state,
-				isLoading: true,
-			};
-		}
+export const fetchShopProducts = createAsyncThunk(
+	"shop/fetchProducts",
+	async () => {
+		const response = await fetch(`http://localhost:3000/products`);
+		const products = await response.json();
+		return products;
+	},
+);
 
-		case "shop/fetchSucceeded": {
-			return {
-				...state,
-				isLoading: false,
-				products: action.payload,
-				errorMessage: "",
-			};
-		}
+// fetchShopProducts.
 
-		case "shop/fetchFailed": {
-			return {
-				...state,
-				isLoading: false,
-				errorMessage: action.payload,
-				products: [],
-			};
-		}
-
-		default: {
-			return state;
-		}
-	}
-};
+export const shopSlice = createSlice({
+	name: "shop",
+	initialState: initState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchShopProducts.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchShopProducts.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.products = action.payload;
+			})
+			.addCase(fetchShopProducts.rejected, (state, action) => {
+				state.isLoading = false;
+				state.errorMessage = action.error.message;
+			});
+	},
+});
